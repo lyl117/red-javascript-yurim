510774ec12c45ce48c7d2b2312e64d7567bb3ff1
yurimlee <ylee981352@gmail.com>
2021-11-17 12:48:43 +0900

    [#3] items firebase 연동 read db연동

---
+++ public/js/items.js
@@ -0,0 +1,83 @@
+let items;
+
+// const queryString = new URLSearchParams(window.location.search);
+// const nameText = queryString.get('input-text');
+// // const inputTextObjects = document.getElementsByName('input-text');
+// // const inputTextObject = inputTextObjects[0];
+// const inputTextObject = document.getElementsByName('input-text')[0]; 
+// inputTextObject.value = nameText;
+// inputTextObject.focus();
+// inputTextObject.blur();
+
+const itemsCreate = function(form) {
+  const itemNameObject = form['item-name'];
+  const item = {
+    name: itemNameObject.value,
+    enter: moment().format('YYYY-MM-DD'),
+    expire: moment().add(3, 'days').format('YYYY-MM-DD')
+  };
+  axios.post('https://red-javascript-yurim-default-rtdb.firebaseio.com/items.json', item).then(function() {
+    itemNameObject.value = '';
+    itemsRead();
+  });
+};
+
+const itemsRead = function() {
+  axios.get('https://red-javascript-yurim-default-rtdb.firebaseio.com/items.json').then(function(response) {
+    items = response.data;
+    const tagTbodyParent = document.getElementById('tag-tbody-parent');
+    tagTbodyParent.innerHTML = '';
+    const tagTrChild = document.getElementById('tag-tr-child');
+    let index = 0;
+    for (let key in items) {
+      const newDivChild = tagTrChild.cloneNode(true);
+      tagTbodyParent.appendChild(newDivChild);
+      // const itemsNameObject = document.getElementsByName('items-name')[index];
+      // itemsNameObject.innerHTML = items[key].name;
+      // const itemsEnterObject = document.getElementsByName('items-enter')[index];
+      // itemsEnterObject.innerHTML = items[key].enter;
+      // const itemsExpireObject = document.getElementsByName('items-expire')[index];
+      // itemsExpireObject.value = items[key].expire;
+      // itemsExpireObject.key = key;
+      // itemsExpireObject.index = index;
+      // const itemsDeleteObject = document.getElementsByName('items-delete')[index];
+      // itemsDeleteObject.key = key;
+      // const itemsCheckboxObject = document.getElementsByName('items-checkbox')[index];
+      // itemsCheckboxObject.key = key;
+      index++;
+    }
+    console.log('Readed', items);
+  });
+};
+
+const itemsDelete = function(key) {
+  const url = 'https://red-javascript-yurim-default-rtdb.firebaseio.com/items/' + key + '.json';
+  axios.delete(url).then(itemsRead);
+};
+
+const itemsUpdate = function(index, key) {
+  const url = 'https://red-javascript-yurim-default-rtdb.firebaseio.com/items/' + key + '.json';
+  const expire = document.getElementsByName('items-expire')[index].value;
+  const item = {
+    expire: expire
+  };
+  axios.patch(url, item).then(itemsRead);
+};
+
+const itemsChange = function(event) {
+  const url = 'https://red-javascript-yurim-default-rtdb.firebaseio.com/items/' + event.target.key + '.json';
+  if (event.target.checked) {
+    console.log('TODO: item 생성')
+    console.log(event.target.key)
+    console.log(items)
+    console.log(items[event.target.key])
+    console.log(event)
+    console.log(url);
+    axios.patch(url, items[event.target.key]);
+  } else {
+    console.log('TODO: item 삭제')
+    axios.delete(url);
+  }
+}
+
+itemsRead();
